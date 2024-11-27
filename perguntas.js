b4c10495667336b4d3535fe9a3be23ef69fce6e0
Vinicius-QuemTeAma <00001096476617sp@al.educacao.sp.gov.br>
2024-11-27 13:42:38 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'MUSICA',
+    'Qual a musica brasileira com mais visualização no mundo?',
+    'A musica brasileira que leva o titulo de musica mais famosa é Bum Bum Tam Tam - Mc Fioti'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual o menor país do mundo?',
+    'O menor país do mundo é o Vaticano com seu 0,44 km² de território e cerca de 800 residentes permanentes.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Conhecimento Geral',
+    'Qual a maior palavra do mundo?',
+    ' Trata-se do nome científico da maior proteína que conhecemos, a titina. Que conta com 189.819 letras.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Cinema',
+    'Qual o filme de parodia mais conhecido no mundo?',
+    'O filme mais conhecido por esse feito é a serie de 5 filmes de "Todo Mundo em Panico".'
 )
